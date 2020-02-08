3ede5ec5a36b44825e8a0593610d7e7fd0acd288
hanting <hanting.hou@gmail.com>
2020-02-08 13:58:34 +0800

    修改缓存获取方式

--- src/caches/menus.rs
+++ src/caches/menus.rs
@@ -1,34 +1,48 @@
 use std::collections::HashMap;
-//use std::sync::Mutex;
+use std::sync::Mutex;
 use fluffy::{db, model::Model};
 use crate::models::Menus;
 
 lazy_static! {
-    pub static ref MENUS: HashMap<usize, String> = {
-        let fields = "id, name";
-        let mut conn = db::get_conn();
-        let cond = cond!["parent_id" => &"0", "is_show" => &"1",];
-        let query = query![fields => &fields,];
-        let rs = Menus::fetch_rows(&mut conn, &query, Some(&cond));
-        let mut menus: HashMap<usize, String> = HashMap::new();
-        for r in rs { 
-            let (id, name): (usize, String) = from_row!(r);
-            menus.insert(id, name);
-        }
-        menus
-    };
+    pub static ref MAIN_MENUS: Mutex<HashMap<usize, String>> = { Mutex::new(get_cache_items()) };
 }
 
 lazy_static! { 
-    pub static ref BREADS: HashMap<String, String> = { 
-        let menus = Menus::get_related();
-        let mut breads: HashMap<String, String> = HashMap::new();
-        for menu in &menus { 
-            for sub in &menu.menus { 
-                let bread = format!("<a href='#'>{}</a> <a href='#'><cite>{}</cite></a>", menu.name, sub.name);
-                breads.insert(sub.url.to_owned(), bread);
-            }
+    pub static ref BREADS: Mutex<HashMap<String, String>> = { Mutex::new(get_cache_breads()) };
+}
+
+/// 缓存刷新
+pub fn refresh() { 
+    // 主菜单
+    let mut main_menus = MAIN_MENUS.lock().unwrap();
+    *main_menus = get_cache_items();
+    // 子菜单
+    let mut breads = BREADS.lock().unwrap();
+    *breads = get_cache_breads();
+}
+
+fn get_cache_items() -> HashMap<usize, String> { 
+    let fields = "id, name";
+    let mut conn = db::get_conn();
+    let cond = cond!["parent_id" => &"0", "is_show" => &"1",];
+    let query = query![fields => &fields,];
+    let rs = Menus::fetch_rows(&mut conn, &query, Some(&cond));
+    let mut menus = HashMap::new();
+    for r in rs { 
+        let (id, name): (usize, String) = from_row!(r);
+        menus.insert(id, name);
+    }
+    menus
+}
+
+fn get_cache_breads() -> HashMap<String, String> { 
+    let menus = Menus::get_related();
+    let mut breads: HashMap<String, String> = HashMap::new();
+    for menu in &menus { 
+        for sub in &menu.menus { 
+            let bread = format!("<a href='#'>{}</a> <a href='#'><cite>{}</cite></a>", menu.name, sub.name);
+            breads.insert(sub.url.to_owned(), bread);
         }
-        breads
-    };
+    }
+    breads
 }
