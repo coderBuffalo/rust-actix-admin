b31857914923eb17b008c66de135ed4acbc6523f
hanting <hanting.hou@gmail.com>
2020-02-10 19:59:17 +0800

    修改后台显示的字段

--- src/models/videos.rs
+++ src/models/videos.rs
@@ -14,11 +14,12 @@ pub struct Videos {
     pub duration: u32, 
     pub state: u32,
     pub created: u32, 
-    pub updated: u32,
+    //pub updated: u32,
     pub category_id: usize,
     pub tag_ids: String,
     pub author_id: usize,
     pub url: String,
+    pub is_recommended: u32,
 }
 
 impl Model for Videos { 
@@ -39,14 +40,15 @@ impl ModelBackend for Videos {
         duration => u32,
         state => u32,
         //created => u32,
-        updated => u32,
+        //updated => u32,
         category_id => usize,
         author_id => usize,
+        is_recommended => u32,
     ]);
 
     fn validate(data: &HashMap<String, String>) -> Result<(), String> { 
         Validator::load(&data)
-            .string_length("title", "标题长度必须在2-30之间", 2, 30, true)
+            .string_length("title", "标题长度必须在2-30之间", 2, 50, true)
             .string_limit("remark", "备注长度不能超过200", 200)
             .string_length("cover_image", "封面地址长度必须在2-200之间", 2, 200, true)
             .is_unsigned("duration", "时长必须是正确的数字")
