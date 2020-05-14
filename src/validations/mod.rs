744ebfad44771e76f64fa19ff69d8fd29d6b26dc
hanting <hanting.hou@gmail.com>
2020-05-14 14:01:01 +0800

    添加注释: 正则表达式

--- src/validations/mod.rs
+++ src/validations/mod.rs
@@ -3,7 +3,9 @@ use regex::Regex;
 use std::str::FromStr;
 
 lazy_static! { 
+    /// 用户名称正则
     static ref RE_USERNAME: Regex = { Regex::new(r"^[a-zA-Z]+[a-zA-Z_0-9]{4,19}$").unwrap() };
+    /// 电子邮件正则
     static ref RE_MAIL: Regex = { Regex::new(r"/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,5})$/").unwrap() };
 }
 
